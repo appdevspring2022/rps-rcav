00e59caa3eb091ce99c09f3566be358508f4a563
Wenshi Wang <wenshi@uchicago.edu>
2022-04-19 21:59:20 +0000

    rock page playing works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    # redirect_to("https://wikipedia.org")
+    # render({ :plain=>"hello world"})
+    # render({ :html=>"<h1>hewssl</h1>".html_safe})
+    render({ :template=>"game_templates/user_rock.html.erb"})
+  end
 end
